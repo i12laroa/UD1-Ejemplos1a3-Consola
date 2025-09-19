3e9f090dd922f6336a2d2ae899b002f088e8920f
i12laroa <albertolamarca@gmail.com>
2025-09-19 15:41:14 +0200

    Agregar archivos de proyecto.

---
+++ UD1-Ej1-Array/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace UD1_Ej1_Array
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de un Array de palabras
+            string[] words = new string[3];
+            int count = 0;
+
+            //Introducimos datos en el array 
+            while (count < words.Length)
+            {
+                Console.WriteLine($"Escribe la palabra número {count+1}:");
+                words[count]=Console.ReadLine();
+                count++;
+            }
+
+            //Mostramos el resultado 
+            Console.WriteLine("Contenido del Array:");
+
+            foreach (string word in words)
+            {
+
+                Console.WriteLine(word);
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+
+           
+
+        }
+    }
+}
---
+++ UD1-Ej1-Array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("UD1-Ej1-Array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UD1-Ej1-Array")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("500f8ca7-c477-4eea-944f-adc8eda50523")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
